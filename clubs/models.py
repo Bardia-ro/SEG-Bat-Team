7895917b7a8d71354369638b0f824a4b31d11f83
Esther Okusanya <estherbokusanya@gmail.com>
2021-11-17 05:32:55 -0800

    Create User Model, Update Settings for user mode authentication

--- clubs/models.py
+++ clubs/models.py
@@ -1,3 +1,15 @@
+from django.core.validators import RegexValidator
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db.models.fields import BLANK_CHOICE_DASH
 
 # Create your models here.
+class User(AbstractUser):
+    first_name = models.CharField(max_length=50, blank=False)
+    last_name = models.CharField(max_length=50, blank=False)
+    email = models.EmailField(unique=True, blank=False)
+    bio = models.CharField(max_length=520, blank=True)
+    experience = models.CharField(black = False)
+    personal_statement = models.CharField(max_field=600, blank=False)
+    status = models.CharField(blank=False)
+
