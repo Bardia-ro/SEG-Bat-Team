d0d863de287c27cca17f95a311fd1302e0572252
Bardia Rokhzadifar <k2036646@kcl.ac.uk>
2021-11-18 19:47:31 +0000

    bug free but not complete models

--- clubs/models.py
+++ clubs/models.py
@@ -1,3 +1,37 @@
+from django.core.validators import RegexValidator
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db.models.deletion import CASCADE
+from django.db.models.lookups import Regex
+#from django.db.models.fields import BLANK_CHOICE_DASH, proxy
+
 
 # Create your models here.
+class User(AbstractUser):
+    bio = models.TextField()
+    # UserTypes = (
+    # ('OFFICER', 'Officer'),
+    # ('CLUBOWNER', 'ClubOwner'),
+    # ('MEMBER', 'Member')
+    # )
+
+    # type = models.CharField(max_length=50, choices=UserTypes, default='MEMBER') 
+
+    # username = models.CharField(
+    #     blank=False,
+    #     max_length=30,
+    #     unique = True,
+    #     validators=[RegexValidator(
+    #         regex=r'^@\w{3,}$',
+    #         message='username must consist of @ followed by at least 3 alphanumerical'
+    #     )]
+
+    # )
+    # first_name = models.CharField(max_length=50, blank=False)
+    # last_name = models.CharField(max_length=50, blank=False)
+    # email = models.EmailField(unique=True, blank=False)
+    # bio = models.CharField(max_length=520, blank=True)
+    # experience = models.CharField(max_length = 520, blank = False)
+    # personal_statement = models.CharField(max_length=600, blank=False)
+
+
--- system/settings.py
+++ system/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'system.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
@@ -124,3 +124,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'clubs.User'
