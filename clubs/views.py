9da60e22b8463299d6a2d0246296d1f38d1972ac
tayy-b <k20059556@kcl.ac.uk>
2021-12-05 23:15:23 +0000

    Fix ability for officers to access pending requests page

--- clubs/views.py
+++ clubs/views.py
@@ -20,10 +20,9 @@ def request_toggle(request, user_id, club_id):
         Role.objects.create(user = currentUser, club = club, role = 1)
 
     user_is_applicant = get_is_user_applicant(club_id, request.user)
-
     user_is_officer = get_is_user_officer(club_id, request.user)
-    club_list = currentUser.get_clubs_user_is_a_member()
-    club_members = Role.objects.filter(club=club, role=2)
+    club_list = Role.objects.filter(user = request.user)
+    club_members = Role.objects.filter(club=club)
     return render(request, 'club_page.html' ,
     {'club_id': club_id,'user_is_applicant':user_is_applicant, 'club': club,'club_list': club_list, 'club_members': club_members, 'user_is_officer':user_is_officer})
 
@@ -133,7 +132,9 @@ def club_page(request, club_id):
     club_members = Role.objects.filter(club=club, role=2)
     user_is_member = get_is_user_member(club_id, request.user)
     role_at_club = request.user.get_role_as_text_at_club(club_id)
-    return render (request, 'club_page.html', {'club_id': club_id,'user_is_member':user_is_member, 'club': club, 'club_list': club_list, 'club_members': club_members, 'role_at_club': role_at_club})
+    user_is_applicant = get_is_user_applicant(club_id, request.user)
+    user_is_officer = get_is_user_officer(club_id, request. user)
+    return render (request, 'club_page.html', {'club_id': club_id,'user_is_applicant': user_is_applicant, 'user_is_officer': user_is_officer, 'user_is_member':user_is_member, 'club': club, 'club_list': club_list, 'club_members': club_members, 'role_at_club': role_at_club})
 
 def member_list(request, club_id):
     if not request.user.get_is_user_associated_with_club(club_id):
@@ -148,7 +149,6 @@ def member_list(request, club_id):
     users = User.objects.filter(club__id = club_id)
     club_list = request.user.get_clubs_user_is_a_member()
     return render(request, 'member_list.html', {'users': users, 'user_is_member': True, 'club_id': club_id, 'club_list': club_list})
-
 
 @login_required
 def approve_member(request, club_id, applicant_id):
@@ -193,5 +193,4 @@ def club_list(request):
 def pending_requests(request,club_id):
     applicants = Role.objects.all().filter(role = 1).filter(club_id = club_id)
     # need applicants for a particular club
-    
     return render(request, 'pending_requests.html', { 'club_id':club_id,'applicants' : applicants })
