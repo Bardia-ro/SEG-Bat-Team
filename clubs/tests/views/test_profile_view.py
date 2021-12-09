2c2800e9067d705d5646bcbd7a8cb594af0772f0
Michael Trent <mtrent133@gmail.com>
2021-12-09 17:17:04 +0000

    Edit/Add fixtures

--- clubs/tests/views/test_profile_view.py
+++ clubs/tests/views/test_profile_view.py
@@ -9,18 +9,18 @@ class ProfileViewTestCase(TestCase):
     fixtures = ["clubs/tests/fixtures/default_user.json", "clubs/tests/fixtures/other_users.json"]
 
     def setUp(self):
-        self.url = reverse('profile', kwargs={"user_id": 0})
+        self.url = reverse('profile', kwargs={"club_id": 0, "user_id": 1})
 
     def test_profile_url(self):
-        self.assertEqual(self.url, '/profile/0')
+        self.assertEqual(self.url, '/profile/0/1')
 
     def test_non_logged_in_user_gets_user_profile_page(self):
         response = self.client.get(self.url, follow=True)
-        expected_url = '/log_in/?next=/profile/0'
+        expected_url = '/log_in/?next=/profile/0/1'
         self.assertRedirects(response, expected_url)
         self.assertTemplateUsed(response, 'log_in.html')
 
-    def test_applicant_user_gets_own_profile_page(self):
+    def test_applicant_user_not_in_any_club_gets_own_profile_page(self):
         self.client.login(email='johndoe@example.org', password='Password123')
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
@@ -29,18 +29,14 @@ class ProfileViewTestCase(TestCase):
         self.assertEqual(response.context['user'], user)   
         self.assertFalse(response.context['user_is_member']) 
         self.assertTrue(response.context['is_current_user'])  
-        self.assertContains(response, user.first_name)
-        self.assertContains(response, user.last_name)
-        self.assertContains(response, user.type)
-        self.assertContains(response, user.email)
-        self.assertContains(response, user.bio)
-        self.assertContains(response, user.experience)
-        self.assertContains(response, user.personal_statement)
-        self.assertContains(response, 'Edit profile')
+        self._assert_response_contains_content(response, user)
+    
+    def test_applicant_user_in_1_club_gets_own_profile_page(self):
+        pass
         
     def test_member_user_gets_own_profile_page(self):
         user = User.objects.get(id=1)
-        url = reverse('profile', kwargs={"user_id": 1})
+        url = reverse('profile', kwargs={"club_id": 0,"user_id": 1})
         self.client.login(email='janedoe@example.org', password='Password123')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
@@ -48,6 +44,9 @@ class ProfileViewTestCase(TestCase):
         self.assertEqual(response.context['user'], user)   
         self.assertTrue(response.context['user_is_member']) 
         self.assertTrue(response.context['is_current_user'])  
+        self._assert_response_contains_content(response, user)
+
+    def _assert_response_contains_content(self, response, user):
         self.assertContains(response, user.first_name)
         self.assertContains(response, user.last_name)
         self.assertContains(response, user.type)
@@ -57,9 +56,12 @@ class ProfileViewTestCase(TestCase):
         self.assertContains(response, user.personal_statement)
         self.assertContains(response, 'Edit profile')
 
+    def test_non_applicant_user_gets_other_user_profile_page(self):
+        pass
+
     def test_applicant_user_gets_other_user_profile_page(self):
         self.client.login(email='johndoe@example.org', password='Password123')
-        url = reverse('profile', kwargs={"user_id": 1})
+        url = reverse('profile', kwargs={""club_id": 0,user_id": 1})
         response = self.client.get(url, follow=True)
         expected_url = reverse('profile', kwargs={"user_id": 0})
         self.assertRedirects(response, expected_url)
@@ -68,18 +70,10 @@ class ProfileViewTestCase(TestCase):
         self.assertEqual(response.context['user'], user)   
         self.assertFalse(response.context['user_is_member']) 
         self.assertTrue(response.context['is_current_user'])  
-        self.assertContains(response, user.first_name)
-        self.assertContains(response, user.last_name)
-        self.assertContains(response, user.type)
-        self.assertContains(response, user.email)
-        self.assertContains(response, user.bio)
-        self.assertContains(response, user.experience)
-        self.assertContains(response, user.personal_statement)
-        self.assertContains(response, 'Edit profile')
 
     def test_member_user_gets_applicant_user_profile_page(self):
         self.client.login(email='janedoe@example.org', password='Password123')
-        url = reverse('profile', kwargs={"user_id": 0})
+        url = reverse('profile', kwargs={"club_id": 0,"user_id": 0})
         response = self.client.get(url, follow=True)
         expected_url = reverse('profile', kwargs={"user_id": 1})
         self.assertRedirects(response, expected_url)
@@ -87,19 +81,11 @@ class ProfileViewTestCase(TestCase):
         user = User.objects.get(email='janedoe@example.org')
         self.assertEqual(response.context['user'], user)   
         self.assertTrue(response.context['user_is_member']) 
-        self.assertTrue(response.context['is_current_user'])  
-        self.assertContains(response, user.first_name)
-        self.assertContains(response, user.last_name)
-        self.assertContains(response, user.type)
-        self.assertContains(response, user.email)
-        self.assertContains(response, user.bio)
-        self.assertContains(response, user.experience)
-        self.assertContains(response, user.personal_statement)
-        self.assertContains(response, 'Edit profile')
+        self.assertTrue(response.context['is_current_user'])
 
     def test_member_user_gets_other_member_profile_page(self):
         self.client.login(email='janedoe@example.org', password='Password123')
-        url = reverse('profile', kwargs={"user_id": 2})
+        url = reverse('profile', kwargs={"club_id": 0,"user_id": 2})
         response = self.client.get(url, follow=True)
         expected_url = reverse('profile', kwargs={"user_id": 1})
         self.assertRedirects(response, expected_url)
@@ -107,12 +93,12 @@ class ProfileViewTestCase(TestCase):
         user = User.objects.get(email='janedoe@example.org')
         self.assertEqual(response.context['user'], user)   
         self.assertTrue(response.context['user_is_member']) 
-        self.assertTrue(response.context['is_current_user'])  
-        self.assertContains(response, user.first_name)
-        self.assertContains(response, user.last_name)
-        self.assertContains(response, user.type)
-        self.assertContains(response, user.email)
-        self.assertContains(response, user.bio)
-        self.assertContains(response, user.experience)
-        self.assertContains(response, user.personal_statement)
-        self.assertContains(response, 'Edit profile')
+        self.assertTrue(response.context['is_current_user'])
+
+    def test_officer_gets_member_profile_page(self):
+        pass
+
+    def test_owner_gets_member_profile_page(self):
+        pass
+
+    
