a5e0c17ed8dddb6d9f618744d6bcad3194dadb80
Michael Trent <mtrent133@gmail.com>
2021-12-11 21:00:47 +0000

    Edit MatchAdmin class

--- clubs/admin.py
+++ clubs/admin.py
@@ -44,5 +44,5 @@ class MatchAdmin(admin.ModelAdmin):
     """Configuration of the admin interface for Matches."""
 
     list_display = [
-        'number','tournament', 'winner', 'player1', 'player2',
+        'number', 'player1', 'player2', 'winner'
     ]
