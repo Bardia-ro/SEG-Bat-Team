afac27dfe025d49b1a8fc3aacb3b0d8a8bf5dcf4
Michael Trent <mtrent133@gmail.com>
2021-12-14 21:26:28 +0000

    Update player's total group points when group match result is submitted

--- clubs/models.py
+++ clubs/models.py
@@ -517,10 +517,18 @@ class GroupMatch(models.Model):
         self.player1_points += 1
         self.save()
 
+        group_points_for_player1 = self.get_group_points_object_for_player1()
+        group_points_for_player1.total_group_points += 1
+        group_points_for_player1.save()
+
     def player2_won_points(self):
         """Set the score of player 2"""
         self.player2_points += 1
         self.save()
+
+        group_points_for_player2 = self.get_group_points_object_for_player2()
+        group_points_for_player2.total_group_points += 1
+        group_points_for_player2.save()
 
     def set_draw_points(self):
         """Updates the score if the match is a draw"""
@@ -528,6 +536,25 @@ class GroupMatch(models.Model):
         self.player2_points += 0.5
         self.save()
 
+        group_points_for_player1 = self.get_group_points_object_for_player1()
+        group_points_for_player1.total_group_points += 0.5
+        group_points_for_player1.save()
+
+        group_points_for_player2 = self.get_group_points_object_for_player2()
+        group_points_for_player2.total_group_points += 0.5
+        group_points_for_player2.save()
+
+    def get_group_points_object_for_player1(self):
+        return GroupPoints.objects.get(
+            group=self.group, 
+            player=self.match.player1
+        )
+
+    def get_group_points_object_for_player2(self):
+        return GroupPoints.objects.get(
+            group=self.group, 
+            player=self.match.player2
+        )
 
 class GroupMatchNextMatches(models.Model):
     group_match = models.ForeignKey(GroupMatch, on_delete=models.CASCADE)
