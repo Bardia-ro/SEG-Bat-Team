a821b42748c900732dc0285b6f2c8a28fbe7197e
Esther Okusanya <estherbokusanya@gmail.com>
2021-12-15 11:15:52 -0800

    Edit Fixtures - IAdd more tests to  club, role and role model

--- clubs/tests/models/test_club_model.py
+++ clubs/tests/models/test_club_model.py
@@ -6,7 +6,8 @@ class ClubTest(TestCase):
 
     fixtures = ['clubs/tests/fixtures/default_user.json',
                 "clubs/tests/fixtures/other_users.json",
-                'clubs/tests/fixtures/default_club.json']
+                'clubs/tests/fixtures/default_club.json',
+                'clubs/tests/fixtures/other_clubs.json']
 
     def setUp(self):
         super(TestCase, self).setUp()
@@ -20,11 +21,39 @@ class ClubTest(TestCase):
     def test_name_must_not_be_blank(self):
         self.club.name = ''
         self.assert_club_is_invalid()
+    
+    def test_name_must_not_be_over_50_characters(self):
+        self.club.name = 'x' *51
+        self.assert_club_is_invalid()
+    
+    def test_name_accepts_50_characters(self):
+        self.club.name = 'x' *50
+        self.assert_club_is_valid()
+    
+    def test_name_must_be_unique(self):
+        self.club.name = 'Club B'
+        self.assert_club_is_invalid()
 
     def test_description_must_not_be_blank(self):
         self.club.description = ''
         self.assert_club_is_invalid()
+
+    def test_description_accepts_600_characters(self):
+        self.club.description = 'x'*600
+        self.assert_club_is_valid()
     
+    def test_description_must_not_be_over_600_characters(self):
+        self.club.description = 'x'*601
+        self.assert_club_is_invalid()
+    
+    def test_city_accepts_255_characters(self):
+        self.club.city = 'x'*255
+        self.assert_club_is_valid()
+    
+    def test_city_must_not_be_over_255_characters(self):
+        self.club.city = 'x'*256
+        self.assert_club_is_invalid()
+
     def assert_club_is_valid(self):
         try:
             self.club.full_clean()
