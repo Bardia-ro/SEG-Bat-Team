a5096bc47ddc76a011f7ed1ce481fd1899a69463
Harry-Keetch <harry.keetch@kcl.ac.uk>
2021-12-16 17:22:09 +0000

    Update main

--- clubs/views.py
+++ clubs/views.py
@@ -14,6 +14,7 @@ from django.contrib import messages
 from .helpers import get_is_user_member, only_current_user, redirect_authenticated_user, get_is_user_applicant, get_is_user_owner, get_is_user_officer
 from django.contrib.auth.mixins import LoginRequiredMixin
 from itertools import chain, count
+from django.contrib.auth.decorators import user_passes_test
 
 def request_toggle(request, user_id, club_id):
 
