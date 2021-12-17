b841c6c2dc1e1ee946badddc71311c748582f2dc
Esther Okusanya <estherbokusanya@gmail.com>
2021-12-16 16:15:13 -0800

    Test apply_leave toggle in club page view

--- clubs/tests/views/test_club_page_view.py
+++ clubs/tests/views/test_club_page_view.py
@@ -11,7 +11,8 @@ class ClubPageTestCase(TestCase, LogInTester):
 
     fixtures = ['clubs/tests/fixtures/default_user.json',
                 'clubs/tests/fixtures/other_users.json',
-                'clubs/tests/fixtures/default_club.json',]
+                'clubs/tests/fixtures/default_club.json',
+                'clubs/tests/fixtures/other_clubs.json']
 
     def setUp(self):
         self.url = reverse('club_page', kwargs={"club_id": 0})
@@ -32,3 +33,33 @@ class ClubPageTestCase(TestCase, LogInTester):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'club_page.html')
         self.assertEqual(response.context['club_id'], 0)
+
+    def test_request_toggle_when_owner_rejects(self):
+        self.client.login(email='johndoe@example.org', password='Password123')
+        user =User.objects.get(id=200)
+        self.assertEqual(user.get_role_as_text_at_club(0), "Owner")
+        url = reverse('request_toggle', kwargs={"user_id": 200, "club_id": 0})
+        response = self.client.get(url, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(user.get_role_as_text_at_club(0), "Owner")
+        messages_list = list(response.context['messages'])
+        self.assertEqual(len(messages_list), 1)
+        self.assertEqual(messages_list[0].level, messages.ERROR)
+    
+    def test_request_toggle_to_leave_club(self):
+        a_member=User.objects.get(pk=2)
+        self.assertEqual(a_member.get_role_as_text_at_club(0), "Member")
+        self.client.login(email=a_member.email, password='Password123')
+        url = reverse('request_toggle', kwargs={"user_id": 2, "club_id": 0})
+        response = self.client.get(url, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(a_member.get_role_as_text_at_club(0), "Not a member")
+
+    def test_request_toggle_to_enter_club(self):
+        non_member=User.objects.get(pk=200)
+        self.assertEqual(non_member.get_role_as_text_at_club(1), "Not a member")
+        self.client.login(email=non_member.email, password='Password123')
+        url = reverse('request_toggle', kwargs={"user_id": 200, "club_id": 1})
+        response = self.client.get(url, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(non_member.get_role_as_text_at_club(1), "Application Pending")
